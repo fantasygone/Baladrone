2b3f7ff10c3d0e3cd2bd930a267220173e3c3b8b
Rolo <alvin3906@gmail.com>
2025-01-11 18:58:25 +0100

    Patch Lift Seal

--- seals/lift_seal.lua
+++ seals/lift_seal.lua
@@ -6,14 +6,6 @@ SMODS.Seal {
     pos = {x= 3, y= 5},
     sound = { sound = 'cs_lift_seal_obtained', per = 1.2, vol = 0.4 },
 
-    loc_txt = {
-        name = "Lift Seal",
-        label = "Lift Seal",
-        text = {
-            "Yes",
-        },
-    },
-
     loc_vars = function(self, info_queue)
         return { vars = {} }
     end,
@@ -22,7 +14,7 @@ SMODS.Seal {
     -- card - card this seal is applied to
     calculate = function(self, card, context)
         -- repetition_only context is used for red seal retriggers
-        if context.repetition and context.repetition_only and context.cardarea == G.play then
+        if context.repetition and context.repetition_only and (context.cardarea == G.play or context.cardarea == G.hand) then
             return {
                 message = localize('cs_lift'),
                 repetitions = 2,
